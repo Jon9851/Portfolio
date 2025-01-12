e6eba2a8ff868a8481487a093acff25f20fe6f29
Jonathan Cohen <91493101+Jon9851@users.noreply.github.com>
2025-01-12 12:58:51 +0000

    changes to resopnsivness, didnt work on small screen sizes correclty footer changed due aligiment issues

--- portfolio/app/components/Footer.jsx
+++ portfolio/app/components/Footer.jsx
@@ -4,42 +4,48 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='mt-10'>
-      <div className='text-center'>
+    <footer className="mt-10 px-6 sm:px-12">
+      <div className="text-center">
+        <h4 className="text-lg font-semibold">Let's Connect</h4>
       </div>
-      
-      <div className='text-center sm:flex items-center justify-between border-t border-black-800 mx-[10%] mt-12 py-6 sm:mt-10'>
-        <div className='flex justify-start'>
-          <span>© Jonathan Cohen 2025.</span>
+
+      <div className="sm:flex items-center justify-between border-t border-gray-800 mt-12 py-6 sm:mt-10">
+        {/* Copyright Section */}
+        <div className="text-center sm:text-left">
+          <span className="text-sm text-gray-600">© Jonathan Cohen 2025.</span>
         </div>
 
-        <div className='flex items-center gap-2'>
-          <Image src={assets.mail_icon} alt='' className='w-6' />
-          <span>joncohen21K@hotmail.co.uk</span>
+        {/* Contact Section */}
+        <div className="flex items-center justify-center gap-2 mt-4 sm:mt-0">
+          <Image src={assets.mail_icon} alt="Email Icon" className="w-6" />
+          <span className="text-sm text-gray-600">joncohen21K@hotmail.co.uk</span>
         </div>
 
-        <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-10'>
+        {/* Social Links */}
+        <ul className="flex items-center justify-center gap-6 mt-6 sm:mt-0">
           <li>
             <a 
-              target='_blank' 
+              target="_blank" 
+              rel="noopener noreferrer" 
               href="https://www.linkedin.com/in/jonathan-cohen-8447ba162/" 
-              className="hover:text-red-500"
+              className="text-sm text-gray-600 hover:text-red-500 transition duration-300"
             >
               LinkedIn
             </a>
           </li>
           <li>
             <a 
-              target='_blank' 
+              target="_blank" 
+              rel="noopener noreferrer" 
               href="https://github.com/Jon9851" 
-              className="hover:text-red-500"
+              className="text-sm text-gray-600 hover:text-red-500 transition duration-300"
             >
               Github
             </a>
           </li>
         </ul>
       </div>
-    </div>
+    </footer>
   );
 };
 
